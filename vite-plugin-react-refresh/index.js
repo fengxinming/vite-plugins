08415d3026d6820f64b40acb587a51168c221dfc
Jesse Feng <fxm0016@126.com>
2021-07-31 12:10:09 +0800

    Update index.js

--- vite-plugin-react-refresh/index.js
+++ vite-plugin-react-refresh/index.js
@@ -1,34 +1,27 @@
 'use strict';
 
-const { EOL } = require('os');
-const { readFileSync, writeFileSync } = require('fs');
+const { existsSync, readFileSync, writeFileSync } = require('fs');
 
-const OVERRIDDEN_FLAG = '/* VITE-PLUGIN-REACT-REFRESH */';
 const TPS_START = '/* TRANSFORM_PLUGINS_START */';
 const TPS_END = '/* TRANSFORM_PLUGINS_END */';
 
-function reactRefresh({ transformPlugins, ...restOpts } = {}) {
+function reactRefresh(opts) {
   const oldReactRefreshPluginPath = require.resolve('@vitejs/plugin-react-refresh');
-  let code = readFileSync(oldReactRefreshPluginPath, 'utf8');
-  const TPSStr = `${TPS_START}${transformPlugins.join(', ')}${TPS_END}`;
+  const newReactRefreshPluginPath = oldReactRefreshPluginPath.replace(/\.js$/, '.plus.js');
 
-  if (!code.includes(OVERRIDDEN_FLAG)) {
-    code = OVERRIDDEN_FLAG + EOL + code.replace(
+  if (!existsSync(newReactRefreshPluginPath)) {
+    let code = readFileSync(oldReactRefreshPluginPath, 'utf8');
+    const TPSStr = `${TPS_START}...((opts && opts.transformPlugins) || [])${TPS_END}`;
+
+    code = code.replace(
       /require\(['"]@babel\/plugin-transform-react-jsx-source['"]\)/,
-      [
-        'require(\'@babel/plugin-transform-react-jsx-source\')',
-        TPSStr
-      ].join(', ')
+      `require('@babel/plugin-transform-react-jsx-source'), ${TPSStr}`
     );
-  }
-  else {
-    code = `${code.slice(0, code.indexOf(TPS_START))}${TPSStr}${code.slice(code.indexOf(TPS_END) + TPS_END.length)}`;
+
+    writeFileSync(newReactRefreshPluginPath, code, 'utf8');
   }
 
-  delete require.cache[oldReactRefreshPluginPath];
-  writeFileSync(oldReactRefreshPluginPath, code, 'utf8');
-
-  return require(oldReactRefreshPluginPath)(restOpts);
+  return require(newReactRefreshPluginPath)(opts);
 }
 
 module.exports = reactRefresh;
