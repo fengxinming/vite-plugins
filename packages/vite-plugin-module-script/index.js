d2061bc65a73aa5b12f1c94e4f58406ca85723f4
Jesse Feng <fxm0016@126.com>
2021-09-07 13:13:33 +0800

    Update index.js

--- packages/vite-plugin-module-script/index.js
+++ packages/vite-plugin-module-script/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
+const { send } = require('vite');
 const crypto = require('crypto');
+const getRouter = require('find-my-way');
 
 const runtimePublicPath = '/@react-refresh';
 const preambleCode = `import RefreshRuntime from "__BASE__${runtimePublicPath.slice(1)}";`
@@ -9,6 +11,18 @@ const preambleCode = `import RefreshRuntime from "__BASE__${runtimePublicPath.sl
 + 'window.$RefreshSig$ = () => (type) => type;'
 + 'window.__vite_plugin_react_preamble_installed__ = true;';
 
+function sanitizeUrl(url) {
+  for (let i = 0, len = url.length; i < len; i++) {
+    const charCode = url.charCodeAt(i);
+    // Some systems do not follow RFC and separate the path and query
+    // string with a `;` character (code 59), e.g. `/foo;jsessionid=123456`.
+    // Thus, we need to split on `;` as well as `?` and `#`.
+    if (charCode === 63 || charCode === 59 || charCode === 35) {
+      return url.slice(0, i);
+    }
+  }
+  return url;
+}
 
 /**
  * create module script string
@@ -19,11 +33,12 @@ const preambleCode = `import RefreshRuntime from "__BASE__${runtimePublicPath.sl
  * @param {object} opts
  * @returns {string}
  */
-function moduleScript(id, base, url, { async, defer }) {
-  if (async) {
+function moduleScript(id, base, url, { inject, async, defer }) {
+  if (inject) {
     let s = `var ${id} = document.getElementById('${id}');`
-        + `${id}&&document.body.removeChild(${id});`
-        + `${id} = document.body.appendChild(document.createElement('script'));`
+      + `var $${inject} = document.querySelector('${inject}');`
+        + `${id}&&$${inject}.removeChild(${id});`
+        + `${id} = $${inject}.appendChild(document.createElement('script'));`
         + `${id}.setAttribute('id', '${id}');`
         + `${id}.setAttribute('type', 'module');`
         + `${id}.setAttribute('src', '${base}${url}');`;
@@ -36,6 +51,9 @@ function moduleScript(id, base, url, { async, defer }) {
       s += `${id}.setAttribute('defer', true);`;
     }
 
+    s += 'var preambleCode = document.head.appendChild(document.createElement(\'script\'));'
+    + `preambleCode.appendChild(document.createTextNode(${preambleCode.replace('__BASE__', base)}));`;
+
     return s;
   }
 
@@ -43,12 +61,29 @@ function moduleScript(id, base, url, { async, defer }) {
     + `document.write('<script id="${id}" type="module" src="${base}${url}"></script>');`;
 }
 
+const router = getRouter();
+
 module.exports = function ({
   async,
   defer,
   mapping
 } = {}) {
+  let keys;
+  if (!mapping || !(keys = Object.keys(mapping)).length) {
+    return null;
+  }
+
   let base = '';
+  keys.forEach((n, i) => {
+    router.on('GET', n, (req, res, params) => {
+      const target = mapping[keys[i]];
+      const id = `m${crypto.createHash('md5').update(target).digest('hex')}`;
+      send(req, res, moduleScript(id, base, target.slice(1), {
+        async,
+        defer
+      }), 'application/javascript');
+    });
+  });
 
   return {
     name: 'module-script',
@@ -63,15 +98,14 @@ module.exports = function ({
     //   console.log(resolvedConfig.base);
     // },
 
-    load(id) {
-      const target = mapping[id];
-      if (target) {
-        const id = `m${crypto.createHash('md5').update(target).digest('hex')}`;
-        return moduleScript(id, base, target.slice(1), {
-          async,
-          defer
-        });
-      }
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        const handle = router.find(req.method, sanitizeUrl(req.url), router.constrainer.deriveConstraints(req));
+        if (handle === null) {
+          return next();
+        }
+        return handle.handler(req, res, handle.params, handle.store);
+      });
     }
   };
 };
