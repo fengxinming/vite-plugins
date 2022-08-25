e82e20f555c49ca79e26595de561fc1bee9cbff0
Jesse Feng <fxm0016@126.com>
2022-08-25 15:48:48 +0800

    Create index.js

---
+++ packages/vite-plugin-reverse-proxy/index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const runtimePublicPath = '/@react-refresh';
+const preambleCode = `import RefreshRuntime from "__BASE__${runtimePublicPath.slice(1)}";
+RefreshRuntime.injectIntoGlobalHook(window);
+window.$RefreshReg$ = () => {};
+window.$RefreshSig$ = () => (type) => type;
+window.__vite_plugin_react_preamble_installed__ = true;`;
+
+function moduleScript(base, url) {
+  // const preambleCodeScript
+  //   = `document.writeln('<script type="module">${preambleCode.replace('__BASE__', base)}</script>');`;
+
+  // const mainScript = `document.write('<script type="module" src="${base}${url}"></script>');`;
+
+  const preambleCodeScript = `
+if (!document.getElementById('preambleCode')) {
+  const preambleCode = document.createElement('script');
+  preambleCode.id = 'preambleCode';
+  preambleCode.type = 'module';
+  preambleCode.appendChild(document.createTextNode(${JSON.stringify(preambleCode.replace('__BASE__', base))}));
+  document.head.insertBefore(preambleCode,document.head.firstChild);
+}
+`;
+
+  const mainScript = `
+const mainScript = document.createElement('script');
+mainScript.type = 'module';
+mainScript.src = '${base}${url}';
+document.body.appendChild(mainScript);
+  `;
+
+  return preambleCodeScript + mainScript;
+}
+
+module.exports = function (mappings = {}) {
+  let devBase = '/';
+  let devMode;
+
+  return {
+    name: 'vite:reverse-proxy',
+
+    config(config, { mode }) {
+      devBase = config.base;
+      devMode = mode;
+    },
+
+    // configResolved(resolvedConfig) {
+    //   console.log(resolvedConfig.base, 'resolvedConfig.base');
+    //   devBase = resolvedConfig.base;
+    // },
+
+    load(id) {
+      if (devMode === 'development') {
+        const target = mappings[id.replace(/(\?|#).*$/, '')];
+        if (target) {
+          return moduleScript(devBase, target);
+        }
+      }
+    }
+  };
+};
