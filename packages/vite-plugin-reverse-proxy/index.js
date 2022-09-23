4d661c52df51b8ef4c0738930d1f4db873a47fd5
Jesse Feng <fxm0016@126.com>
2022-09-23 09:38:33 +0800

    Create index.js

--- packages/vite-plugin-reverse-proxy/index.js
+++ packages/vite-plugin-reverse-proxy/index.js
@@ -7,35 +7,40 @@ window.$RefreshReg$ = () => {};
 window.$RefreshSig$ = () => (type) => type;
 window.__vite_plugin_react_preamble_installed__ = true;`;
 
-function moduleScript(base, url) {
+function moduleScript(base, url, hasReactRefresh) {
   // const preambleCodeScript
   //   = `document.writeln('<script type="module">${preambleCode.replace('__BASE__', base)}</script>');`;
 
   // const mainScript = `document.write('<script type="module" src="${base}${url}"></script>');`;
 
-  const preambleCodeScript = `
-if (!document.getElementById('preambleCode')) {
-  const preambleCode = document.createElement('script');
-  preambleCode.id = 'preambleCode';
-  preambleCode.type = 'module';
-  preambleCode.appendChild(document.createTextNode(${JSON.stringify(preambleCode.replace('__BASE__', base))}));
-  document.head.insertBefore(preambleCode,document.head.firstChild);
-}
-`;
-
-  const mainScript = `
+  let mainScript = `
 const mainScript = document.createElement('script');
 mainScript.type = 'module';
 mainScript.src = '${base}${url}';
 document.body.appendChild(mainScript);
   `;
 
-  return preambleCodeScript + mainScript;
+  if (hasReactRefresh) {
+    const preambleCodeScript = `
+    if (!document.getElementById('preambleCode')) {
+      const preambleCode = document.createElement('script');
+      preambleCode.id = 'preambleCode';
+      preambleCode.type = 'module';
+      preambleCode.appendChild(document.createTextNode(${JSON.stringify(preambleCode.replace('__BASE__', base))}));
+      document.head.insertBefore(preambleCode,document.head.firstChild);
+    }
+    `;
+
+    mainScript = preambleCodeScript + mainScript;
+  }
+
+  return mainScript;
 }
 
 module.exports = function (mappings = {}) {
   let devBase = '/';
   let devMode;
+  let resolvedConfig;
 
   return {
     name: 'vite:reverse-proxy',
@@ -45,16 +50,19 @@ module.exports = function (mappings = {}) {
       devMode = mode;
     },
 
-    // configResolved(resolvedConfig) {
-    //   console.log(resolvedConfig.base, 'resolvedConfig.base');
-    //   devBase = resolvedConfig.base;
-    // },
+    configResolved(config) {
+      resolvedConfig = config;
+    },
 
     load(id) {
       if (devMode === 'development') {
         const target = mappings[id.replace(/(\?|#).*$/, '')];
         if (target) {
-          return moduleScript(devBase, target);
+          return moduleScript(
+            devBase,
+            target,
+            !!resolvedConfig.plugins.find((n) => n.name === 'vite:react-refresh')
+          );
         }
       }
     }
