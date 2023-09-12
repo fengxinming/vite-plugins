a2b0fed36bfabafd700f1c390bc35965596a02b1
Jesse Feng <fxm0016@126.com>
2023-09-12 11:26:25 +0800

    feat: react demo vite config

--- examples/react/vite.config.js
+++ examples/react/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import vitePluginExternal from '../../packages/vite-plugin-external';
 import vitePluginCp from '../../packages/vite-plugin-cp';
+import vitePluginIncludeCSS from '../../packages/vite-plugin-include-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -20,7 +21,8 @@ export default defineConfig({
         { src: './node_modules/vite/dist', dest: 'dist/test2', flatten: false },
         { src: './node_modules/vite/README.md', dest: 'dist' }
       ]
-    })
+    }),
+    vitePluginIncludeCSS()
   ],
   server: {
     open: true
