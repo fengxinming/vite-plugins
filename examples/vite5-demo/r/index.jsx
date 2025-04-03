c4ee6881378dccb7fd3e85e8ac8c2fdfa891d69b
Jesse.Feng <fxm0016@126.com>
2025-04-04 01:25:48 +0800

    ci(github action): 修改发布相关

--- examples/vite5-demo/r/index.jsx
+++ examples/vite5-demo/r/index.jsx
@@ -1,4 +1,4 @@
-import './index.css';
+import '../index.css';
 
 import React, { useState, version } from 'react';
 import ReactDOM from 'react-dom';
--- packages/vite-plugin-external/src/common/constants.ts
+++ packages/vite-plugin-external/src/common/constants.ts
@@ -1,4 +1,4 @@
 import pkg from '../../package.json';
 
 export const PLUGIN_NAME = pkg.name;
-export const ESBUILD_PLUGIN_NAME = `${PLUGIN_NAME}-resolve`;
+export const ESBUILD_PLUGIN_NAME = 'esbuild-plugin-resolve';
